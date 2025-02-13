d36eab61480f8c7c8fff2f8e0e7039790b0f5434
Azharsalam <azharsalam137@gmail.com>
2025-02-13 12:59:43 +0530

    Update Q6.sql

--- MySQL Queries/Q6.sql
+++ MySQL Queries/Q6.sql
@@ -1,9 +1,7 @@
-SELECT
-    MONTHNAME(date) AS month_name, 
-    GROUP_CONCAT(DISTINCT post_category ORDER BY post_category) AS post_category_names, 
-    COUNT(DISTINCT post_category) AS post_category_count 
+SELECT 
+    MONTHNAME(date) AS month_name,
+    GROUP_CONCAT(DISTINCT post_category SEPARATOR ', ') AS post_category_names,
+    COUNT(DISTINCT post_category) AS post_category_count
 FROM fact_content
-GROUP BY 
-    month_name -- Group by month name
-ORDER BY 
-    month_name; 
+GROUP BY MONTH(date), MONTHNAME(date)
+ORDER BY MONTH(date);
